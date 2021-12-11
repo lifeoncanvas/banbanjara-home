5512bf7c13cf8b8035c8e5073e6e1fe79fbc6f85
lifeoncanvas <zoedalvi1902@gmail.com>
2021-12-11 23:29:11 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      bla
+    </div>
+  );
+}
